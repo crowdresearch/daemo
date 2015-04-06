816df75ab0b13a49cd7cbddd523f1a6585bcdf4b
Gordon Wintrob <gwintrob@gmail.com>
2015-04-05 22:13:54 -0700

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn turingmachine.wsgi --log-file -
---
+++ turingmachine/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for turingmachine project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turingmachine.settings")
+
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
