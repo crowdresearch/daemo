40705143d83c4c40b3e4702b7e0f03c549dcf2e3
Durim Morina <drm.mrn@gmail.com>
2015-04-20 15:17:31 +0200

    removed imports that are not used

--- crowdsourcing/models.py
+++ crowdsourcing/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractBaseUser
+from django.contrib.auth.models import User
 from datetime import datetime
 from django.utils import timezone
 
