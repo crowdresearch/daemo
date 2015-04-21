1ef562b19b735ca69a5058bf3872227fc532c319
nistala <nrohits@gmail.com>
2015-04-21 15:10:35 -0700

    loading bar and alerts for login

--- staticfiles/js/authentication/services/authentication.service.js
+++ staticfiles/js/authentication/services/authentication.service.js
@@ -68,25 +68,7 @@
     function login(email, password) {
       return $http.post('/api/v1/auth/login/', {
         username: email, password: password
-      }).then(loginSuccessFn, loginErrorFn);
-
-      /**
-       * @name loginSuccessFn
-       * @desc Set the authenticated account and redirect to index
-       */
-      function loginSuccessFn(data, status, headers, config) {
-        Authentication.setAuthenticatedAccount(data.data);
-
-        window.location = '/';
-      }
-
-      /**
-       * @name loginErrorFn
-       * @desc Log "Epic failure!" to the console
-       */
-      function loginErrorFn(data, status, headers, config) {
-        console.error('Epic failure!');
-      }
+      });
     }
 
     /**
