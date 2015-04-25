c646221b0f6ff18da2f9dec62c1709df5878b59c
Durim Morina <drm.mrn@gmail.com>
2015-04-25 15:04:52 +0200

    oauth2 client and tokens split login controller changed to request tokens on success getOauth2Token and setOauth2Token added new routes for ng requests to oauth2backend

--- staticfiles/js/authentication/controllers/login.controller.js
+++ staticfiles/js/authentication/controllers/login.controller.js
@@ -44,8 +44,22 @@
       Authentication.login(vm.email, vm.password).then(function success(data, status) {
       
         Authentication.setAuthenticatedAccount(data.data);
-        $window.location = '/home'
-      
+        //$window.location = '/home'
+            Authentication.getOauth2Token(vm.email, vm.password,
+                "password", data.data.client_id, data.data.client_secret).then(function success(data, status) {
+                Authentication.setOauth2Token(data.data);
+                $window.location = '/home'
+              }, function error(data, status) {
+
+            $alert({
+              title: 'Error getting OAUTH2!',
+              content: data.data.message,
+              placement: 'top',
+              type: 'danger',
+              keyboard: true,
+              duration: 5});
+
+          });
       }, function error(data, status) {
       
         $alert({
