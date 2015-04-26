b76f3b42ba12a5d23fe4b8626ab5934d6174b1d1
neilthemathguy <avrim.mit@gmail.com>
2015-04-25 21:17:02 -0400

    Added url for the RequesterRanking api, organized the imports.

--- csp/urls.py
+++ csp/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
-
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib import admin
 admin.autodiscover()
 from crowdsourcing import views
 from crowdsourcing import views as api_views
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls) ),
