f81466ca0e193f280fba095d97f0857cbb818839
neilthemathguy <avrim.mit@gmail.com>
2015-04-29 02:07:56 -0400

    Added the json with new url

--- staticfiles/js/ranking/controllers/ranking.controller.js
+++ staticfiles/js/ranking/controllers/ranking.controller.js
@@ -1,27 +1,32 @@
 //@TODO move to serivces 
+__author__ = 'neilthemathguy'
+
 var rankingApp = angular.module('crowdsource.ranking.controllers', ['ngGrid']);
 
 rankingApp.controller('RankingController', function($scope, $log, $http) {	
     	$scope.selections = [];
     	$scope.ranking = [];
-    	$scope.gridOptions = {
-    	data:'ranking',
-        selectedItems: $scope.selections,
-    	multiSelect: false,
-    	enablePinning: true,
-        columnDefs: [{ field:"Name", width: 200, pinned: true },
-                    { field: "Payment", width: 100 },
-                    { field: "Fairness", width: 100 },
-                    { field: "OperationalSpeed", width: 150 },
-                    { field: "Communication", width: 150 },
-                    { field: "TotalReviews", width: 140 }
-                    ]
-    	}
-    	
-    	$http.get("/requesterranking").success(function(data,config) {
-            console.log(data)
+    	$http.get("/api-auth/v1/auth/requesterranking/?format=json").success(function(data,config) {
         	$scope.ranking = data
+        	console.log($scope.ranking )
     }).error(function(data, status, headers, config) {
            console.log(status)
     });
+    
+    $scope.gridOptions = {
+    selectedItems: $scope.mySelections,
+    multiSelect: false,
+    enablePinning: true,
+    data:'ranking',
+    columnDefs: [
+                    { field: "requester_name", displayName: 'Requester Name', width:220,pinned: true },
+                    { field:"requester_communicationRank",displayName: 'Communication', width:140 },
+                    { field: "requester_fairRank", displayName:'Fairness', width:100 },
+                    { field: "requester_payRank", displayName: 'Payment', width: 100 },
+                    { field: "requester_speedRank", displayName: 'Operational Speed', width: 130 },
+                    { field: "requester_numberofReviews", displayName: 'Total Reviews',  width: 40 },
+                    ]
+    };	
 }) 
+
+
