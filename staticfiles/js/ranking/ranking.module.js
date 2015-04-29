68e35fa9a5cd7b1808b610e9184efc0c9a2cdbf5
neilthemathguy <avrim.mit@gmail.com>
2015-04-29 00:59:23 -0400

    Added ranking module

---
+++ staticfiles/js/ranking/ranking.module.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('crowdsource.ranking', [
+      'crowdsource.ranking.controllers',      
+      //'crowdsource.ranking.services',
+
+    ]);
+
+  angular
+    .module('crowdsource.ranking.controllers', []);
+  
+//@TODO decouple the the service
+//  angular
+//  .module('crowdsource.ranking.services', []);
+})();
