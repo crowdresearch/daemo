20e632dec008ee8922a681e3e24d692409736c94
neilthemathguy <avrim.mit@gmail.com>
2015-05-03 18:34:19 -0400

    Moved under js

---
+++ staticfiles/js/tasksearch/tasksearch.module.js
@@ -0,0 +1,18 @@
+//__author__ = 'neilthemathguy'
+
+(function () {
+  'use strict';
+
+  angular
+    .module('crowdsource.tasksearch', [
+      'crowdsource.tasksearch.controllers',      
+    ]);
+
+  angular
+    .module('crowdsource.tasksearch.controllers', []);
+  
+})();
+
+
+
+
