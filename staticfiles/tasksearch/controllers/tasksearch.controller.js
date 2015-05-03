2c8fd8ff38e146388491bd961266d79e91b322af
neilthemathguy <avrim.mit@gmail.com>
2015-05-03 18:33:12 -0400

    Added snipest for the controller.

---
+++ staticfiles/tasksearch/controllers/tasksearch.controller.js
@@ -0,0 +1,9 @@
+//__author__ = 'neilthemathguy'
+
+var taskSearchApp = angular.module('crowdsource.tasksearch.controllers', ['smart-table']);
+
+taskSearchApp.controller('taskSearchGridController', ['$scope', function ($scope) {
+							
+    //Add the http.get to fetchrecords (example see task.controller.js)                    	   
+
+}]);                  
