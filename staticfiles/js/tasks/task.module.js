479577d5417dae8f007f86ada7444e9367488653
neilthemathguy <avrim.mit@gmail.com>
2015-05-03 21:11:01 -0400

    Adding task module

---
+++ staticfiles/js/tasks/task.module.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('crowdsource.tasks', [
+      'crowdsource.tasks.controllers',      
+    ]);
+
+  angular
+    .module('crowdsource.tasks.controllers', []);
+  
+})();
