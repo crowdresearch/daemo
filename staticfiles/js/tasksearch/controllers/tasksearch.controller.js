a2547e542b3c8485b8430b6f9189306ba771ed32
neilthemathguy <avrim.mit@gmail.com>
2015-05-03 20:55:03 -0400

    Added name of the API to call

--- staticfiles/js/tasksearch/controllers/tasksearch.controller.js
+++ staticfiles/js/tasksearch/controllers/tasksearch.controller.js
@@ -3,7 +3,7 @@
 var taskSearchApp = angular.module('crowdsource.tasksearch.controllers', ['smart-table']);
 
 taskSearchApp.controller('taskSearchGridController', ['$scope', function ($scope) {
-							
+	///API for http get call: api/module/?format=json						
     //Add the http.get to fetchrecords (example see task.controller.js)                    	   
 
 }]);                  
