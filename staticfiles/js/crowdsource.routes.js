2a2d6111eaf7706d331e7019e266894a3d4a339d
nistala <nrohits@gmail.com>
2015-05-09 11:32:30 -0700

    contributor pages

--- staticfiles/js/crowdsource.routes.js
+++ staticfiles/js/crowdsource.routes.js
@@ -14,32 +14,32 @@
   function config($routeProvider) {
     $routeProvider.when('/', {
       templateUrl: '/static/templates/intro.html',
-    }).otherwise('/')
+    })
 
     .when('/home', {
       templateUrl: '/static/templates/home.html',
       controller: 'HomeController',
-    }).otherwise('/')
+    })
 
     .when('/ranking', {
       templateUrl: '/static/templates/ranking/requesterrank.html',
       controller: 'RankingController',
-    }).otherwise('/')
+    })
     
     //We will merge tasklistSearch and tasklist to one component, please keep it separate for now.
     .when('/tasklistSearch', {
     templateUrl: '/static/templates/tasksearches/tasklistSearch.html',
     controller: 'taskSearchGridController',
-    }).otherwise('/')
+    })
     
     .when('/tasklist', {
       templateUrl: '/static/templates/task/tasklist.html',
       controller: 'taskController',
-    }).otherwise('/')
+    })
     
-      .when('/ImageLabel', {
-        templateUrl: '/static/templates/task/ImageLabel.html'
-      }).otherwise('/')
+    .when('/ImageLabel', {
+      templateUrl: '/static/templates/task/ImageLabel.html'
+    })
       
     .when('/register', {
       controller: 'RegisterController',
@@ -51,14 +51,27 @@
       controller: 'LoginController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/authentication/login.html'
-    }).otherwise('/')
+    })
 
     .when('/profile', {
       templateUrl: '/static/templates/home.html'
-    }).otherwise('/')
+    })
     
     .when('/terms', {
       templateUrl: '/static/templates/terms.html'
-    }).otherwise('/');   
+    })
+
+    .when('/contributors', {
+      templateUrl: '/static/templates/contributors/home.html'
+    })
+
+  /**
+   * Location for contributor urls. Include your personal page url here.
+   */
+    .when('/contributors/rohit', {
+      templateUrl: '/static/templates/contributors/rohit.html'
+    })
+
+    .otherwise('/');   
   }
 })();
