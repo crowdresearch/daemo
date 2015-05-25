fba1f4e2d344029dd3e4dd167c8cc3cc42d35bc6
nistala <nrohits@gmail.com>
2015-05-25 16:27:16 -0700

    revert staticfiles

--- staticfiles/js/ranking/controllers/ranking.controller.js
+++ staticfiles/js/ranking/controllers/ranking.controller.js
@@ -4,13 +4,27 @@
 var rankingApp = angular.module('crowdsource.ranking.controllers', ['ngGrid']);
 
 rankingApp.controller('RankingController', function($scope, $log, $http) {	
-    	$scope.rankings = [];
+    	$scope.ranking = [];
     	$http.get("/api/requesterranking/?format=json").success(function(data,config) {
-        	$scope.rankings = data
-        	console.log($scope.rankings)
+        	$scope.ranking = data
+        	console.log($scope.ranking )
         }).error(function(data, status, headers, config) {
                console.log(status)
         });
+    	
+    $scope.gridOptions = {
+    multiSelect: false,
+    enablePinning: true,
+    data:'ranking',
+    columnDefs: [   
+                    { field: "requester_name", displayName: 'Requester Name', width:220,pinned: true },
+                    { field:"requester_communicationRank",displayName: 'Communicativity', width:140 },
+                    { field: "requester_fairRank", displayName:'Fairness', width:100 },
+                    { field: "requester_payRank", displayName: 'Generosity', width: 100 },
+                    { field: "requester_speedRank", displayName: 'Promptness', width: 150 },
+                    { field: "requester_numberofReviews", displayName: 'Total Reviews',  width: 40 }
+                    ]
+    };	
 }) 
 
 
--- staticfiles/js/tasksearch/controllers/tasksearch.controller.js
+++ staticfiles/js/tasksearch/controllers/tasksearch.controller.js
@@ -17,4 +17,18 @@ taskSearchApp.controller('taskSearchGridController', ['$scope','$http','$filter'
 	    }).error(function(data, status, headers, config) {
 	           console.log(status);
 	    });
+
+	    $scope.gridOptionsTask = {
+	    multiSelect: false,
+	    enablePinning: true,
+	    data:'task',
+	    columnDefs: [   { field: "name", displayName: 'Name', width: 200, pinned: true },
+	                    { field: "description", displayName: 'Description', width:150 },
+	                    { field:"keywords",displayName: 'Keywords', width:190 },
+	                    { field: "module_timeout", displayName:'Hours Remain', width:130 },
+	                    { field: "repetition", displayName: 'Repetition', width: 100 },
+	                    { field: "price", displayName: 'Pay', width: 60 },
+	                    ]
+	    };
+
 }]);                  
