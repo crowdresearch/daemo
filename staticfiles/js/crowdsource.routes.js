15309a67254b3a1aeab9276fb3b8e465c4829025
Rolando Cruz <pspnando@me.com>
2015-05-28 18:47:59 -0700

    added myself to contributors

--- staticfiles/js/crowdsource.routes.js
+++ staticfiles/js/crowdsource.routes.js
@@ -25,33 +25,33 @@
       templateUrl: '/static/templates/ranking/requesterrank.html',
       controller: 'RankingController',
     })
-    
+
     //We will merge tasklistSearch and tasklist to one component, please keep it separate for now.
     .when('/tasklistSearch', {
     templateUrl: '/static/templates/tasksearches/tasklistSearch.html',
     controller: 'taskSearchGridController',
     })
-    
+
     .when('/requester', {
       templateUrl: '/static/templates/requester/home.html',
       controller: 'HomeController',
     })
-    
+
     .when('/tasklist', {
       templateUrl: '/static/templates/task/tasklist.html',
       controller: 'taskController',
     })
-    
+
     .when('/ImageLabel', {
       templateUrl: '/static/templates/task/ImageLabel.html'
     })
-      
+
     .when('/register', {
       controller: 'RegisterController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/authentication/register.html'
     })
-    
+
     .when('/login', {
       controller: 'LoginController',
       controllerAs: 'vm',
@@ -61,7 +61,7 @@
     .when('/profile', {
       templateUrl: '/static/templates/home.html'
     })
-    
+
     .when('/terms', {
       templateUrl: '/static/templates/terms.html'
     })
@@ -97,7 +97,7 @@
 
       templateUrl: '/static/templates/contributors/shirish.html'
     })
-    
+
     .when('/contributors/ard', {
       templateUrl: '/static/templates/contributors/ard.html'
     })
@@ -113,19 +113,19 @@
     .when('/contributors/sean', {
       templateUrl: '/static/templates/contributors/sean.html'
     })
-    
+
      .when('/contributors/leek', {
       templateUrl: '/static/templates/contributors/leek.html'
     })
- 
+
     .when('/contributors/karan', {
       templateUrl: '/static/templates/contributors/karan.html'
     })
-    
+
     .when('/contributors/vaidehi', {
       templateUrl: '/static/templates/contributors/vaidehi.html'
     })
-    
+
     .when('/contributors/jason', {
       templateUrl: '/static/templates/contributors/jason.html'
     })
@@ -137,16 +137,19 @@
     .when('/contributors/arcolife', {
       templateUrl: '/static/templates/contributors/arcolife.html'
     })
-     
+
 	.when('/contributors/AditiNath', {
       templateUrl: '/static/templates/contributors/aditinath.html'
     })
-	
+
 	.when('/contributors/AmitRakesh', {
 		templateUrl: '/static/templates/contributors/amitrakesh.html'
 	})
-      
-	 
-    .otherwise('/');   
+
+  .when('/contributors/MisterCruz', {
+    templateUrl: '/static/templates/contributors/mistercruz.html'
+})
+
+    .otherwise('/');
   }
 })();
