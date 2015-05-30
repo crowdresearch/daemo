f97ee9dfdf1c9c8647b091ec59c38fa40a721e50
Rolando Cruz <pspnando@me.com>
2015-05-30 14:59:47 -0700

    pr 190

--- staticfiles/js/crowdsource.routes.js
+++ staticfiles/js/crowdsource.routes.js
@@ -25,33 +25,33 @@
       templateUrl: '/static/templates/ranking/requesterrank.html',
       controller: 'RankingController',
     })
-    
+
     //We will merge tasklistSearch and tasklist to one component, please keep it separate for now.
     .when('/tasklistSearch', {
     templateUrl: '/static/templates/tasksearches/tasklistSearch.html',
     controller: 'taskSearchGridController',
     })
-    
+
     .when('/requester', {
       templateUrl: '/static/templates/requester/home.html',
       controller: 'HomeController',
     })
-    
+
     .when('/tasklist', {
       templateUrl: '/static/templates/task/tasklist.html',
       controller: 'taskController',
     })
-    
+
     .when('/ImageLabel', {
       templateUrl: '/static/templates/task/ImageLabel.html'
     })
-      
+
     .when('/register', {
       controller: 'RegisterController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/authentication/register.html'
     })
-    
+
     .when('/login', {
       controller: 'LoginController',
       controllerAs: 'vm',
@@ -61,7 +61,7 @@
     .when('/profile', {
       templateUrl: '/static/templates/home.html'
     })
-    
+
     .when('/terms', {
       templateUrl: '/static/templates/terms.html'
     })
@@ -98,10 +98,12 @@
       templateUrl: '/static/templates/contributors/shirish.html'
     })
 
+
     .when('/contributors/geza', {
       templateUrl: '/static/templates/contributors/geza.html'
     })
-    
+
+
     .when('/contributors/ard', {
       templateUrl: '/static/templates/contributors/ard.html'
     })
@@ -121,19 +123,19 @@
     .when('/contributors/sean', {
       templateUrl: '/static/templates/contributors/sean.html'
     })
-    
+
      .when('/contributors/leek', {
       templateUrl: '/static/templates/contributors/leek.html'
     })
- 
+
     .when('/contributors/karan', {
       templateUrl: '/static/templates/contributors/karan.html'
     })
-    
+
     .when('/contributors/vaidehi', {
       templateUrl: '/static/templates/contributors/vaidehi.html'
     })
-    
+
     .when('/contributors/jason', {
       templateUrl: '/static/templates/contributors/jason.html'
     })
@@ -145,7 +147,22 @@
     .when('/contributors/arcolife', {
       templateUrl: '/static/templates/contributors/arcolife.html'
     })
-     
+
+	.when('/contributors/AditiNath', {
+      templateUrl: '/static/templates/contributors/aditinath.html'
+    })
+
+	.when('/contributors/AmitRakesh', {
+		templateUrl: '/static/templates/contributors/amitrakesh.html'
+	})
+
+  .when('/contributors/MisterCruz', {
+    templateUrl: '/static/templates/contributors/mistercruz.html'
+})
+
+    .otherwise('/');
+
+
     .when('/contributors/AditiNath', {
       templateUrl: '/static/templates/contributors/aditinath.html'
     })
@@ -157,31 +174,38 @@
     .when('/contributors/AmitRakesh', {
 		templateUrl: '/static/templates/contributors/amitrakesh.html'
     })
-	
+
     .when('/contributors/Milstein', {
       templateUrl: '/static/templates/contributors/milstein.html'
     })
 
     .when('/contributors/bmoix', {
       templateUrl: '/static/templates/contributors/bmoix.html'
-    }) 
-	
+
+    })
+
     .when('/contributors/vineet', {
+
+    })
+
+	.when('/contributors/vineet', {
+
       templateUrl: '/static/templates/contributors/vineet.html'
     })
-    
+
     .when('/contributors/damon', {
       templateUrl: '/static/templates/contributors/damon.html'
-    })        
-	
+    })
+
 	.when('/contributors/rcompton', {
       templateUrl: '/static/templates/contributors/rcompton.html'
     })
 
+
     .when('/contributors/niraj', {
       templateUrl: '/static/templates/contributors/niraj.html'
     })
-    
+
     .when('/contributors/abhinav', {
       templateUrl: '/static/templates/contributors/abhinav.html'
     })
@@ -189,11 +213,11 @@
 	.when('/contributors/Pabitra', {
       templateUrl: '/static/templates/contributors/Pabitra.html'
     })
-    
+
   	.when('/contributors/KajalGupta', {
       templateUrl: '/static/templates/contributors/kajal.html'
     })
-    
+
     .when('/contributors/pemmasani', {
       templateUrl: '/static/templates/contributors/pemmasani.html'
     })
@@ -205,7 +229,7 @@
     .when('/contributors/saviaga', {
       templateUrl: '/static/templates/contributors/saviaga.html'
     })
- 
+
     .when('/contributors/pitkofsky', {
       templateUrl: '/static/templates/contributors/pitkofsky.html'
     })
@@ -217,14 +241,22 @@
     .when('/contributors/msb', {
       templateUrl: '/static/templates/contributors/msb.html'
     })
-    
+
     .when('/contributors/ankita2992', {
       templateUrl: '/static/templates/contributors/ankita.html'
     })
-    
+
     .when('/contributors/savage', {
       templateUrl: '/static/templates/contributors/savage.html'
     })
-    .otherwise('/');   
+    .otherwise('/');
+
+    .when('/contributors/mistercruz', {
+    	templateUrl: '/static/templates/contributors/mistercruz.html'
+    })
+
+
+
+    .otherwise('/');
   }
 })();
