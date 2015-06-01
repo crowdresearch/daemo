1366b3a8a8b6aaf75e834faf87bd71c651a41661
asmitagupta <guptaasmita.ag@gmail.com>
2015-06-01 20:08:37 +0530

    Update task.py

--- crowdsourcing/viewsets/task.py
+++ crowdsourcing/viewsets/task.py
@@ -2,3 +2,9 @@ class TaskViewSet(viewsets.ModelViewSet):
     from crowdsourcing.models import Task
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    
+
+class QualificationViewSet(viewsets.ModelViewSet):
+    from crowdsourcing.models import Qualification
+    queryset = Qualification.objects.all()
+    serializer_class = QualificationSerializer
