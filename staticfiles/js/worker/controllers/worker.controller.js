c966cc94384d86d13443895f717b98bfe1c7b075
radhikabhanu <radhika.bhanu@inmobi.com>
2015-06-04 13:29:55 +0530

    worker folder

---
+++ staticfiles/js/worker/controllers/worker.controller.js
@@ -0,0 +1,90 @@
+/**
+* HomeController
+* @namespace crowdsource.home.controllers
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('crowdsource.worker.controllers')
+    .controller('WorkerProfileController', WorkerProfileController);
+
+  WorkerProfileController.$inject = ['$location', '$scope', 'Authentication'];
+
+  /**
+  * @namespace HomeController
+  */
+  function WorkerProfileController($location, $scope, Authentication) {
+    var vm = this;
+    //Static mock JSON for worker homepage
+    $scope.user = {
+      rank : 45,
+      totalProjects : 13,
+      currentProjects : 10,
+      earnings: '50,000',
+      upcomingDeadline : 'July 10',
+      realTimeTaskProgress : [{
+          name : 'Image Labeling',
+          completed : false 
+        },
+        {
+          name : 'Sentiment Analysis',
+          completed : true 
+        },
+        {
+          name : 'Academic Survey',
+          completed : true 
+        }
+      ],
+      skills : [
+        {
+          name : 'Data Entry',
+          total : 120
+        },
+        {
+          name : 'Analysis',
+          total : 30
+        },
+        {
+          name : 'Writing',
+          total : 50
+        }
+      ],
+      approvedTasks : [
+        {
+          name : 'Data Entry'
+        },
+        {
+          name : 'Writing'
+        },
+        {
+          name : 'Image Labeling'
+        }
+      ],
+      bookmarkedRequesters : [
+        {
+          name : 'MIT'
+        },
+        {
+          name : 'Media Lab'
+        },
+        {
+          name : 'Social Computing Lab'
+        }
+      ],
+      socialLinks : {
+          fb : 'http://facebook.com/testProfile',
+          twitter : 'http://twitter.com/testProfile',
+          gplus : 'plus.google.com/testProfile'
+      }
+    };
+    var numberOfRealTimeTasks = $scope.user.realTimeTaskProgress.length, count=0;
+    for(var i=0; i<numberOfRealTimeTasks; i++) {
+      if($scope.user.realTimeTaskProgress[i].completed == true)
+        {
+          count++;
+        }
+    }
+    $scope.progress = parseInt(count/numberOfRealTimeTasks *100).toFixed(2);
+  }
+})();
---
+++ staticfiles/js/worker/worker.module.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('crowdsource.worker', [
+      'crowdsource.worker.controllers',
+    ]);
+
+  angular
+    .module('crowdsource.worker.controllers', []);
+
+})();
