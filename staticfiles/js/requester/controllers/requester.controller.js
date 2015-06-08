2dc730f97e5066439d255d87bfc206ed6c5f3518
Karan Rajpal <karan.rajpal@inmobi.com>
2015-06-08 17:54:03 +0530

    Introduced Requester service, along with http call

--- staticfiles/js/requester/controllers/requester.controller.js
+++ staticfiles/js/requester/controllers/requester.controller.js
@@ -9,103 +9,16 @@
     .module('crowdsource.requester.controllers')
     .controller('RequesterProfileController', RequesterProfileController);
 
-  RequesterProfileController.$inject = ['$location', '$scope', 'Authentication'];
+  RequesterProfileController.$inject = ['$location', '$scope', 'Authentication','Requester'];
 
   /**
   * @namespace RequesterProfileController
   */
-  function RequesterProfileController($location, $scope, Authentication) {
+  function RequesterProfileController($location, $scope, Authentication, Requester) {
     var vm = this;
-    $scope.requesterProfile = {
-     rank : 45,
-     upcomingDeadline : 'July 10',
-     summary : [
-       {
-         name : 'Total Projects',
-         amount : 400
-       },
-       {
-         name : 'Current Projects',
-         amount : 20
-       },
-       {
-         name : 'Waiting for Approval',
-         amount : 10
-       },
-     ],
-     realTimeTaskProgress : [{
-         name : 'Image Labeling',
-         completed : false,
-         percent : 72
-       },
-       {
-         name : 'Sentiment Analysis',
-         completed : false,
-         percent : 33
-       },
-       {
-         name : 'Academic Survey',
-         completed : true,
-         percent : 100
-       }
-     ],
-     paymentActivities : [
-       {
-         name : 'Survey',
-         amount : 120
-       },
-       {
-         name : 'Writing',
-         amount : 50
-       },
-       {
-         name : 'Total',
-         amount : 170
-       },
-     ],
-     approvedTasks : [
-       {
-         name : 'Data Entry'
-       },
-       {
-         name : 'Writing'
-       },
-       {
-         name : 'Image Labeling'
-       }
-     ],
-     bookmarkedWorkers : [
-       {
-         name : 'John Lennon'
-       },
-       {
-         name : 'George Harrison'
-       },
-       {
-         name : 'Paul McCartney'
-       }
-     ],
-     remainingBalance : [
-       {
-         name : 'Survey',
-         amount : 12
-       },
-       {
-         name : 'Writing',
-         amount : 5
-       },
-       {
-         name : 'Total',
-         amount : 17
-       },
-     ],
-     socialLinks : {
-         fb : 'http://facebook.com/testProfile',
-         twitter : 'http://twitter.com/testProfile',
-         gplus : 'plus.google.com/testProfile'
-     }
-   };
 
-
+    Requester.getRequesterPrivateProfile().success(function(data) {
+      $scope.requesterProfile = data;
+    });
   }
 })();
--- staticfiles/js/requester/requester.module.js
+++ staticfiles/js/requester/requester.module.js
@@ -4,9 +4,13 @@
   angular
     .module('crowdsource.requester', [
       'crowdsource.requester.controllers',
+      'crowdsource.requester.services'
     ]);
 
   angular
     .module('crowdsource.requester.controllers', []);
+
+  angular
+    .module('crowdsource.requester.services', []);
 
 })();
