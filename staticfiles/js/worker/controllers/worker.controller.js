ff2b0e3cfa2820d2854f3e1a946bb89f220e25be
radhikabhanu <radhika.bhanu@inmobi.com>
2015-06-08 17:41:29 +0530

    worker profile with factory

--- staticfiles/js/worker/controllers/worker.controller.js
+++ staticfiles/js/worker/controllers/worker.controller.js
@@ -9,82 +9,25 @@
     .module('crowdsource.worker.controllers')
     .controller('WorkerProfileController', WorkerProfileController);
 
-  WorkerProfileController.$inject = ['$location', '$scope', 'Authentication'];
+  WorkerProfileController.$inject = ['$location', '$scope', 'Authentication', 'Worker'];
 
   /**
-  * @namespace HomeController
+  * @namespace WorkerProfileController
   */
-  function WorkerProfileController($location, $scope, Authentication) {
+  function WorkerProfileController($location, $scope, Authentication, Worker) {
     var vm = this;
-    //Static mock JSON for worker homepage
-    $scope.user = {
-      rank : 45,
-      totalProjects : 13,
-      currentProjects : 10,
-      earnings: '50,000',
-      upcomingDeadline : 'July 10',
-      realTimeTaskProgress : [{
-          name : 'Image Labeling',
-          completed : false 
-        },
-        {
-          name : 'Sentiment Analysis',
-          completed : true 
-        },
-        {
-          name : 'Academic Survey',
-          completed : true 
-        }
-      ],
-      skills : [
-        {
-          name : 'Data Entry',
-          total : 120
-        },
-        {
-          name : 'Analysis',
-          total : 30
-        },
-        {
-          name : 'Writing',
-          total : 50
-        }
-      ],
-      approvedTasks : [
-        {
-          name : 'Data Entry'
-        },
-        {
-          name : 'Writing'
-        },
-        {
-          name : 'Image Labeling'
-        }
-      ],
-      bookmarkedRequesters : [
-        {
-          name : 'MIT'
-        },
-        {
-          name : 'Media Lab'
-        },
-        {
-          name : 'Social Computing Lab'
-        }
-      ],
-      socialLinks : {
-          fb : 'http://facebook.com/testProfile',
-          twitter : 'http://twitter.com/testProfile',
-          gplus : 'plus.google.com/testProfile'
-      }
-    };
-    var numberOfRealTimeTasks = $scope.user.realTimeTaskProgress.length, count=0;
-    for(var i=0; i<numberOfRealTimeTasks; i++) {
+    Worker.getWorkerPrivateProfile().success(function(data) {
+      $scope.user = data;
+      var numberOfRealTimeTasks = $scope.user.realTimeTaskProgress.length, count=0;
+      for(var i=0; i<numberOfRealTimeTasks; i++) {
       if($scope.user.realTimeTaskProgress[i].completed == true)
         {
           count++;
         }
-    }
-    $scope.progress = parseInt(count/numberOfRealTimeTasks *100).toFixed(2);
+      }
+      $scope.progress = parseInt(count/numberOfRealTimeTasks *100).toFixed(2);
+    });
+    
+    
   }
 })();
--- staticfiles/js/worker/worker.module.js
+++ staticfiles/js/worker/worker.module.js
@@ -4,9 +4,13 @@
   angular
     .module('crowdsource.worker', [
       'crowdsource.worker.controllers',
+       'crowdsource.worker.services'
     ]);
 
   angular
     .module('crowdsource.worker.controllers', []);
+
+  angular
+    .module('crowdsource.worker.services', ['ngCookies']);
 
 })();
