dca11554a188e44061be8c3159a8c68bbcbbadee
agarwal22megha <agarwal22megha@gmail.com>
2015-06-10 00:11:51 +0100

    Updating Worker REST API Adding additional functionalities to Worker Profile

--- crowdsourcing/serializers/worker.py
+++ crowdsourcing/serializers/worker.py
@@ -1,18 +1,123 @@
 __author__ = 'elsabakiu, dmorina, neilthemathguy, megha, asmita'
 
 from crowdsourcing import models
-from rest_framework import serializers
-
-class WorkerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Worker
-
+from rest_framework import serializers, status
+from datetime import datetime
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Skill
         fields = ('name', 'description', 'verified', 'deleted', 'created_timestamp', 'last_updated')
         read_only_fields = ('created_timestamp', 'last_updated')
+
+    def create(self, validated_data):
+        skill = models.Skill.objects.create(deleted=False, **validated_data)
+        return skill
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        #TODO(megha.agarwal): Define method to verify the skill added
+        instance.verified = True
+        instance.save()
+        return instance
+
+    def delete(self, instance):
+        instance.deleted = True
+        instance.save()
+        return instance
+
+
+#Return difference between start_date and end_date in hours
+def date_diff_inHours (start_date, end_date):
+    date1 = datetime.strptime(start_date, "%Y-%m-%d")
+    date2 = datetime.strptime(end_date, "%Y-%m-%d")
+    return abs((date1 - date2).hour)
+
+
+class WorkerSerializer(serializers.ModelSerializer):
+    num_tasks = serializers.SerializerMethodField()
+    task_status_det = serializers.SerializerMethodField()
+    task_category_det = serializers.SerializerMethodField()
+    task_price_time = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Worker
+        fields = ('profile', 'skills', 'num_tasks', 'task_status_det', 'task_category_det', 'task_price_time')
+        read_only_fields = ('num_tasks', 'task_status_det', 'task_category_det', 'task_price_time')
+
+    def create(self, validated_data):
+        skill_set = validated_data.pop('skills')
+        worker = models.Worker.objects.create(deleted=False, **validated_data)
+        for get_skill in skill_set:
+            models.WorkerSkill.objects.create(worker = worker, skill = get_skill)
+        return worker
+
+    # Returns number of tasks the worker has/had worked on
+    def get_num_tasks(self):
+        taskWorker = models.TaskWorker()
+        response_data = taskWorker.objects.filter(worker = self).count()
+        return response_data
+
+    # Returns tasks grouped by task status that the worker has undertaken
+    # Also returns the number of tasks within each task status
+    def get_task_status_det(self):
+        task_status = dict()
+        number_task_per_status = dict()
+        taskWorker = models.TaskWorker()
+        task_set =  taskWorker.objects.filter(worker = self)
+
+        # e.g. task_status = {'Accepted': ['Task1', 'Task2', 'Task3']}
+        for task in task_set:
+            key = task.module.statuses
+            value = task.module.description
+            task_status.setdefault(key, [])
+            task_status[key].append(value)
+
+        # e.g. number_task_per_status = ['Accepted' : 3]
+        for key_status in task_status:
+            number_task_per_status[key_status] = len(task_status[key_status])
+
+        return task_status, number_task_per_status
+
+    # Returns the task grouped by Category that the worker has undertaken
+    # Also returns the number of tasks within each category
+    def get_task_category_det(self):
+        task_categories = dict()
+        number_task_per_category = dict()
+        taskWorker = models.TaskWorker()
+        task_set = taskWorker.objects.filter(worker = self)
+
+        # e.g. task_categories = {'Image': ['Task1', 'Task2', 'Task3']}
+        for task in task_set:
+            key = task.module.categories.caregory.name
+            value = task.module.description
+            task_categories.setdefault(key, [])
+            task_categories[key].append(value)
+
+        # e.g. number_task_per_category = ['Image' : 3]
+        for key_category in task_categories:
+            number_task_per_category[key_category] = len(task_categories[key_category])
+
+        return task_categories, number_task_per_category
+
+    #Returns the number of hours spent by a worker on the task and corresponding price
+    def get_task_price_time(self):
+        task_det = []
+        taskWorker = models.TaskWorker()
+        task_set = taskWorker.objects.filter(worker = self)
+        # e.g. task_det = [{description: 'Task1', price: '50$', time_spent_in_hrs: '2', deadline: '2015-06-11'}]
+        for task in task_set:
+            task_info = dict()
+            deadline = task.module.project.deadline
+            #TODO(megha.agarwal): Refine duration spent on a task
+            time_spent = date_diff_inHours (task.created_timestamp - task.last_updated)
+            task_info['description'] = task.module.description
+            task_info['deadline'] = deadline
+            task_info['price'] = task.module.price
+            task_info['time_spent_in_hrs'] = time_spent
+            task_det.append(task_info)
+        return task_det
 
 
 class WorkerSkillSerializer(serializers.ModelSerializer):
--- crowdsourcing/viewsets/worker.py
+++ crowdsourcing/viewsets/worker.py
@@ -1,42 +1,134 @@
-__author__ = 'dmorina, asmita'
+__author__ = 'dmorina, asmita, megha'
 
 from crowdsourcing.serializers.worker import *
-from rest_framework import viewsets
-
-class WorkerSkillViewSet(viewsets.ModelViewSet):
-    from crowdsourcing.models import WorkerSkill
-    queryset = WorkerSkill.objects.all()
-    serializer_class = WorkerSkillSerializer
-
-
-class WorkerViewSet(viewsets.ModelViewSet):
-    from crowdsourcing.models import Worker
-    queryset = Worker.objects.all()
-    serializer_class = WorkerSerializer
-
+from crowdsourcing.models import *
+from rest_framework import status, viewsets, mixins
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 class SkillViewSet(viewsets.ModelViewSet):
-    from crowdsourcing.models import Skill
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
 
+    @detail_route(methods=['post'])
+    def update_category(self, request, id = None):
+        skill_serializer = SkillSerializer(data=request.data)
+        skill = self.get_object()
+        if skill_serializer.is_valid():
+            skill_serializer.update(skill,skill_serializer.validated_data)
+            return Response({'status': 'Updated Skills'})
+        else:
+            return Response(skill_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request, *args, **kwargs):
+        try:
+            skill = self.queryset
+            skill_serializer = SkillSerializer(skill, many = True)
+            return Response(skill_serializer.data)
+        except:
+            return Response([])
+
+    def destroy(self, request, *args, **kwargs):
+        skill_serializer = SkillSerializer()
+        skill = self.get_object()
+        skill_serializer.delete(skill)
+        return Response({'Status': 'Deleted Skills'})
+
+
+class WorkerViewSet(viewsets.ModelViewSet):
+    queryset = Worker.objects.all()
+    serializer_class = WorkerSerializer
+
+    @detail_route(methods=['post'], permission_classes=[IsAuthenticated])
+    def update_project(self, request, pk=None):
+        worker_serializer = WorkerSkill(data=request.data)
+        worker = self.get_object()
+        if worker_serializer.is_valid():
+            worker_serializer.update(worker,worker_serializer.validated_data)
+            return Response({'status': 'Updated Worker'})
+        else:
+            return Response(worker_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request, *args, **kwargs):
+        try:
+            worker = Worker.objects.all()
+            worker_serializer = WorkerSkill(worker, many=True)
+            return Response(worker_serializer.data)
+        except:
+            return Response([])
+
+    def destroy(self, request, *args, **kwargs):
+        worker_serializer = WorkerSerializer()
+        worker = self.get_object()
+        worker_serializer.delete(worker)
+        return Response({'status': 'Deleted Worker'})
+
+    @list_route(methods=['post'])
+    def get_num_tasks(self, request):
+        serializer = WorkerSerializer()
+        response_data = serializer.get_num_tasks(request)
+        return Response(response_data)
+
+    @list_route(methods=['post'])
+    def get_tasksStatus_numTaskPerStatus(self, request):
+        serializer = WorkerSerializer()
+        task_status, number_task_per_status = serializer.get_tasksStatus_numTaskPerStatus(request)
+        return Response(task_status, number_task_per_status)
+
+    @list_route(methods=['post'])
+    def get_taskCategory_numTaskPerCategory(self, request):
+        serializer = WorkerSerializer()
+        task_categories, number_task_per_category = serializer.get_taskCategory_numTaskPerCategory(request)
+        return Response(task_categories, number_task_per_category)
+
+    @list_route(methods=['post'])
+    def get_taskPrice_numHoursSpent(self, request):
+        serializer = WorkerSerializer()
+        task_det = serializer.get_taskPrice_numHoursSpent(request)
+        return Response(task_det)
+
+
+class WorkerSkillViewSet(viewsets.ModelViewSet):
+    queryset = WorkerSkill.objects.all()
+    serializer_class = WorkerSkillSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        worker = get_object_or_404(self.queryset, worker=request.worker)
+        serializer = WorkerSkillSerializer(instance=worker)
+        return Response(serializer.data)
+
 
 class TaskWorkerViewSet(viewsets.ModelViewSet):
-    from crowdsourcing.models import TaskWorker
     queryset = TaskWorker.objects.all()
     serializer_class = TaskWorkerSerializer
 
+    def retrieve(self, request, *args, **kwargs):
+        worker = get_object_or_404(self.queryset, worker=request.worker)
+        serializer = TaskWorkerSerializer(instance=worker)
+        return Response(serializer.data)
+
 
 class TaskWorkerResultViewSet(viewsets.ModelViewSet):
-    from crowdsourcing.models import TaskWorkerResult
     queryset = TaskWorkerResult.objects.all()
     serializer_class = TaskWorkerResultSerializer
 
+    def retrieve(self, request, *args, **kwargs):
+        worker = get_object_or_404(self.queryset, worker=request.worker)
+        serializer = TaskWorkerResultSerializer(instance=worker)
+        return Response(serializer.data)
+
 
 class WorkerModuleApplicationViewSet(viewsets.ModelViewSet):
-    from crowdsourcing.models import WorkerModuleApplication
     queryset = WorkerModuleApplication.objects.all()
     serializer_class = WorkerModuleApplicationSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        worker = get_object_or_404(self.queryset, worker=request.worker)
+        serializer = WorkerModuleApplicationSerializer(instance=worker)
+        return Response(serializer.data)
 
 
 
