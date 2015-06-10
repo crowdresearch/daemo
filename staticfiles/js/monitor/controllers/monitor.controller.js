3c47d09932e4c49c6bbfbc4dbc00ad457da725a4
Ryo Suzuki <ryosuzk@gmail.com>
2015-06-09 18:44:59 -0700

    Enable to sort table when clicked

--- staticfiles/js/monitor/controllers/monitor.controller.js
+++ staticfiles/js/monitor/controllers/monitor.controller.js
@@ -19,12 +19,26 @@
     var vm = $scope;
     vm.workers = [];
     vm.workers = Monitor.getWorkers();
+    vm.filter = undefined;
+    vm.order = undefined;
+
     vm.showModal = showModal;
     vm.getPercent = getPercent;
+    vm.orderTable = orderTable;
+    vm.selectStatus = selectStatus;
     vm.getStatusName = getStatusName;
     vm.getStatusColor = getStatusColor;
 
+    function selectStatus (status) {
+      vm.filter = vm.filter !== status ? status : undefined ;
+    }
+
+    function orderTable (key) {
+      vm.order = vm.order === key ? '-'+key : key;
+    }
+
     function getPercent (workers, status) {
+      status |= 0;
       var complete = workers.filter( function (worker) {
         return worker.status == status;
       })
--- staticfiles/js/monitor/services/monitor.service.js
+++ staticfiles/js/monitor/services/monitor.service.js
@@ -54,7 +54,7 @@
         { name: 'Ryo', status: 0, start: '2015-06-08', time: 10},
         { name: 'Michael', status: 1, start: '2015-06-08', time: 33},
         { name: 'Geza', status: 0, start: '2015-06-08', time: 57},
-        { name: 'Rajan', status: 2, start: '2015-06-08', time: 0},
+        { name: 'Rajan', status: 1, start: '2015-06-08', time: 0},
         { name: 'Neil', status: 2, start: '2015-06-08', time: 4},
         { name: 'Joy', status: 1, start: '2015-06-08', time: 26},
         { name: 'Niloufar', status: 0, start: '2015-06-08', time: 48},
