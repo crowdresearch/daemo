51eceb93778d120c77f4e3fcea505358b9aa95c9
Durim Morina <drm.mrn@gmail.com>
2015-06-13 10:01:59 +0200

    added categories

--- staticfiles/js/project/controllers/project.controller.js
+++ staticfiles/js/project/controllers/project.controller.js
@@ -22,9 +22,17 @@
       self.endDate = null;
       self.name = null;
       self.description = null;
-      self.categories = '';
+      self.selectedCategories = [];
+      self.categories = [{name:'Programming', id:0},{name:'Painting', id:1},{name:'Design & Multimedia', id:2},
+          {name:'Image Labelling', id:3},{name:'Writing', id:4}, {name:'Translation', id:5},
+        {name:'Legal', id:6},{name:'Engineering', id:7}, {name:'Other', id:8}];
       self.getPath = function(){
           return $location.path();
+      };
+      self.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) list.splice(idx, 1);
+        else list.push(item);
       };
       self.categoryPool = ('Programming Painting Design Image-Labelling Writing')
           .split(' ').map(function (category) { return { name: category }; });
