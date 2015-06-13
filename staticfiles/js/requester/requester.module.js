6eea8973c2f4bbd49720ea7590ce74b773e229f3
Karan Rajpal <karan.rajpal@inmobi.com>
2015-06-13 19:07:52 +0530

    Fixed requester progress animation

--- staticfiles/js/requester/requester.module.js
+++ staticfiles/js/requester/requester.module.js
@@ -8,7 +8,18 @@
     ]);
 
   angular
-    .module('crowdsource.requester.controllers', []);
+    .module('crowdsource.requester.controllers', [])
+    .directive('animm',function() {
+      return {
+            link: function(scope, el, attrs){
+                setTimeout(function(){
+                    var progressBar = el[0].getElementsByClassName('progress-bar')[0];
+                    var progressBarWidth = progressBar.getAttribute('aria-valuenow');
+                    progressBar.style.width = progressBarWidth + "%";
+                }, 0);
+            }
+          }
+    });
 
   angular
     .module('crowdsource.requester.services', []);
