5a972b9cd746bfad3097fde72073bdd444555dde
Rohit <rnistala@stanford.edu>
2015-06-13 11:58:19 -0700

    Revert "Revert "Oauth complete""

--- staticfiles/js/project/services/project.service.js
+++ staticfiles/js/project/services/project.service.js
@@ -9,14 +9,14 @@
     .module('crowdsource.project.services')
     .factory('Project', Project);
 
-  Project.$inject = ['$cookies', '$http', '$q', '$location'];
+  Project.$inject = ['$cookies', '$http', '$q', '$location', 'HttpService'];
 
   /**
   * @namespace Project
   * @returns {Factory}
   */
 
-  function Project($cookies, $http, $q, $location) {
+  function Project($cookies, $http, $q, $location, HttpService) {
     /**
     * @name Project
     * @desc The Factory to be returned
@@ -38,8 +38,8 @@
     * @returns {Promise}
     * @memberOf crowdsource.project.services.Project
     */
-    function addProject(project) {
-      return $http({
+    function addProject(name, startDate, endDate, description) {
+      var settings = {
         url: '/api/project/',
         method: 'POST',
         data: {
@@ -50,7 +50,8 @@
           keywords: project.keywords,
           categories: project.categories
         }
-      });
+      };
+      return HttpService.doRequest(settings);
     }            
     function toggle(item) {
           var idx = selectedCategories.indexOf(item);
