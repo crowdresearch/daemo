c9ea02430d3bb719bf80d4d579190a137d551246
shriyanka <shriyanka26@gmail.com>
2015-06-15 16:17:51 +0530

    changes in task.py

--- crowdsourcing/serializers/task.py
+++ crowdsourcing/serializers/task.py
@@ -5,20 +5,13 @@ from rest_framework import serializers
 
 class TaskSerializer(serializers.ModelSerializer):
   deleted = serializers.BooleanField(read_only=True)
-  status=serializers.IntegerField()
+  statuses = ((1, "Created"),(2, 'Accepted'),(3, 'Reviewed'),(4, 'Finished'))
+  status=serializers.ChoiceField(choices=statuses,default=1)
   created_timestamp = serializers.DateTimeField(read_only=True)
-
-  def create(self, **kwargs):
-    task = models.Task.objects.create(deleted = False,**self.validated_data)
-    worker = validated_data.pop('Worker')
-    for t in task:
-      models.TaskWorker.objects.create(task=t,worker=worker)
-    return task
   
   def update(self, instance, validated_data):
-    module = validated_data.pop('Module')
+    module = validated_data.pop('module')
     instance.status = validated_data.get('status', instance.status)
-    instance.statuses = validated_data.get('statuses', instance.statuses)
     instance.save()
     return instance
 
@@ -27,10 +20,10 @@ class TaskSerializer(serializers.ModelSerializer):
     instance.save()
     return instance
 
-class Meta:
-  model = models.Task
-  fields = ('module', 'statuses', 'status', 'deleted', 'created_timestamp', 'last_updated')
-  read_only_fields = ('module', 'statuses', 'created_timestamp', 'last_updated')
+  class Meta:
+    model = models.Task
+    fields = ('module','status', 'deleted', 'created_timestamp', 'last_updated')
+    read_only_fields = ('created_timestamp', 'last_updated')
 
 class CurrencySerializer(serializers.ModelSerializer):
   class Meta:
