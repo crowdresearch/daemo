cdad5f776cbdd440563d1bd4399031fdf57953c5
Durim Morina <drm.mrn@gmail.com>
2015-06-19 22:13:18 +0200

    sidenav togglers

--- staticfiles/js/home/controllers/home.controller.js
+++ staticfiles/js/home/controllers/home.controller.js
@@ -9,12 +9,23 @@
     .module('crowdsource.home.controllers')
     .controller('HomeController', HomeController);
 
-  HomeController.$inject = ['$location', '$scope', 'Authentication'];
+  HomeController.$inject = ['$location', '$scope', 'Authentication', '$mdSidenav', '$mdUtil'];
 
   /**
   * @namespace HomeController
   */
-  function HomeController($location, $scope, Authentication) {
-    var vm = this;
+  function HomeController($location, $scope, Authentication, $mdSidenav, $mdUtil) {
+    var self = this;
+    self.sideNavToggler = sideNavToggler;
+
+      function sideNavToggler(navID) {
+      var debounceFn =  $mdUtil.debounce(function(){
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+          });
+        },300);
+        return debounceFn;
+      }
   }
 })();
