4ce72e0641920b22a9613b5ca236bc2676bfc608
Durim Morina <drm.mrn@gmail.com>
2015-06-25 16:34:07 +0200

    task feed extended

--- staticfiles/js/task-feed/controllers/task-feed.controller.js
+++ staticfiles/js/task-feed/controllers/task-feed.controller.js
@@ -17,5 +17,35 @@
   */
   function TaskFeedController($window, $location, $scope, TaskFeed, $filter) {
       var self = this;
+      self.modules = [
+          {
+              name: "Build a website using AngularJs",
+              description: "Build a website using Python Django Framework, AngularJs and Material Design",
+              category: "Programming",
+              skills: "html, angularjs, python, django, css",
+              tasks: 32
+          },
+          {
+              name: "Help me write a research paper",
+              description: "Topic of the paper is crowd sourcing platforms...",
+              tasks: 23,
+              category: "Writing",
+              skills: "English, research"
+          },
+          {
+              name: "Proof read my paper",
+              description: "A 10 page paper about dragons",
+              tasks: 13,
+              category: "Reading",
+              skills: "English"
+          },
+          {
+              name: "Label galaxies",
+              description: "Label images of galaxies using the predefined options",
+              tasks: 47,
+              category: "Data Science",
+              skills: "None required"
+          }
+      ];
   }
 })();
