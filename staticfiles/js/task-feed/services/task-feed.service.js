83f9b2724efcb570bce66a75fd25a996bf4c9333
Durim Morina <drm.mrn@gmail.com>
2015-06-25 15:48:11 +0200

    task feed service added

--- staticfiles/js/task-feed/services/task-feed.service.js
+++ staticfiles/js/task-feed/services/task-feed.service.js
@@ -21,38 +21,11 @@
     * @name TaskFeed
     * @desc The Factory to be returned
     */
-    var selectedCategories = [];
     var TaskFeed = {
-      addProject: addProject,
-      toggle: toggle,
-      selectedCategories: selectedCategories,
       getCategories: getCategories
     };
 
     return TaskFeed;
-
-
-    /**
-    * @name addProject
-    * @desc Try to create a new project
-    * @returns {Promise}
-    * @memberOf crowdsource.project.services.TaskFeed
-    */
-    function addProject(project) {
-      var settings = {
-        url: '/api/project/',
-        method: 'POST',
-        data: {
-          name: project.name,
-          start_date: project.startDate,
-          end_date: project.endDate,
-          description: project.description,
-          keywords: project.keywords,
-          categories: project.categories
-        }
-      };
-      return HttpService.doRequest(settings);
-    }
 
     function getCategories(){
       return $http({
