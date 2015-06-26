db6478b5901ae41aca41c7447550d5d7e8575a3d
neilthemathguy <avrim.mit@gmail.com>
2015-06-26 04:32:22 -0400

    added fields to the static records

--- staticfiles/js/task-feed/controllers/task-feed.controller.js
+++ staticfiles/js/task-feed/controllers/task-feed.controller.js
@@ -23,29 +23,40 @@
               description: "Build a website using Python Django Framework, AngularJs and Material Design",
               category: "Programming",
               skills: "html, angularjs, python, django, css",
-              tasks: 32
+              tasks: 32,
+              requester: 'Alan',
+              pay: '$15/hr'
+
+
           },
           {
               name: "Help me write a research paper",
               description: "Topic of the paper is crowd sourcing platforms...",
               tasks: 23,
               category: "Writing",
-              skills: "English, research"
+              skills: "English, research",
+              requester: 'Matt',
+              pay: '$7/hr'
           },
           {
               name: "Proof read my paper",
               description: "A 10 page paper about dragons",
               tasks: 13,
               category: "Reading",
-              skills: "English"
+              skills: "English",
+              requester: 'Peter',
+              pay: '$5/hr' 	  
           },
           {
               name: "Label galaxies",
               description: "Label images of galaxies using the predefined options",
               tasks: 47,
               category: "Data Science",
-              skills: "None required"
+              skills: "None required",
+              requester: 'Neo',
+              pay: '$12/hr' 	  
           }
       ];
   }
+  
 })();
