3aeee02a24f6b9a207fb96ddeae34460882c152d
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-07-02 09:32:22 +0530

    removing non ready fields

--- crowdsourcing/serializers/requester.py
+++ crowdsourcing/serializers/requester.py
@@ -8,30 +8,23 @@ class RequesterSerializer(serializers.ModelSerializer):
     total_projects = serializers.SerializerMethodField()
     current_projects = serializers.SerializerMethodField()
     waiting_projects = serializers.SerializerMethodField()
-    upcoming_deadline = serializers.SerializerMethodField()
 
     def get_total_projects(self, model):
+        #TODO : also show count of projects, in which requester is collaborator
         return model.project_set.count()
 
     def get_current_projects(self, model):
-        return models.Module.objects.all().filter(project = model, status = 3, deleted = False).distinct('project')
+        #TODO : also show count of projects, in which requester is collaborator
+        return models.Module.objects.all().filter(project__owner = model, status = 3, deleted = False).distinct('project').count()
 
     def get_waiting_projects(self, model):
-        return models.Module.objects.all().filter(project = model, status = 2, deleted = False).distinct('project')
-
-    def get_upcoming_deadline(self, model):
-        return models.Module.objects.all().filter(deadline__gt = datetime.datetime.now().date()).aggregate(deadline = Min('deadline')).get('deadline')
-
-    def get_active_tasks(self, model):
-        return 0
-
-    def get_recently_approved(self, model):
-        return 0
+        #TODO : also show count of projects, in which requester is collaborator
+        return models.Module.objects.all().filter(project__owner = model, status = 2, deleted = False).distinct('project').count()
 
     class Meta:
         model = models.Requester
-        fields = ('id','profile','total_projects','current_projects','waiting_projects','upcoming_deadline')
-        read_only_fields = ('id','total_projects','current_projects','waiting_projects','upcoming_deadline')
+        fields = ('id','profile','total_projects','current_projects','waiting_projects')
+        read_only_fields = ('id','total_projects','current_projects','waiting_projects')
 
 
 class RequesterRankingSerializer(serializers.ModelSerializer):
