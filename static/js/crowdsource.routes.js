f75c18271bfe86b3c284980340de397ae65b0daa
Shirish Goyal <shirish.goyal@gmail.com>
2015-07-12 17:44:33 +0530

    Contributors page corrected

--- static/js/crowdsource.routes.js
+++ static/js/crowdsource.routes.js
@@ -27,11 +27,11 @@
       templateUrl: '/static/templates/messages/base.html'
     })
     .when('/profile', {
-      templateUrl: '/static/templates/worker/account-basic-info.html',
+      templateUrl: '/static/templates/worker/account-basic-info.html'
     })
     
     .when('/userskills', {
-      templateUrl: '/static/templates/worker/account-skills.html',
+      templateUrl: '/static/templates/worker/account-skills.html'
     })
 
     .when('/worker', {
@@ -165,212 +165,11 @@
     .when('/bookmarked-projects', {
       templateUrl: '/static/templates/task-feed/bookmarked.html'
     })
-    .when('/contributors/rohit', {
-      templateUrl: '/static/templates/contributors/rohit.html'
+    .when('/contributors/:contributorName', {
+      templateUrl: function(params){
+          return '/static/templates/contributors/'+params.contributorName+'.html';
+      }
     })
-
-    .when('/contributors/amx', {
-      templateUrl: '/static/templates/contributors/amx.html'
-    })
-
-    .when('/contributors/aneesha', {
-      templateUrl: '/static/templates/contributors/aneesha.html'
-    })
-
-    .when('/contributors/ansuman', {
-      templateUrl: '/static/templates/contributors/ansuman.html'
-    })
-
-
-    .when('/contributors/anirudh', {
-      templateUrl: '/static/templates/contributors/anirudh.html'
-    })
-
-  	.when('/contributors/shirish', {
-      templateUrl: '/static/templates/contributors/shirish.html'
-    })
-
-    .when('/contributors/geza', {
-      templateUrl: '/static/templates/contributors/geza.html'
-    })
-
-    .when('/contributors/ard', {
-      templateUrl: '/static/templates/contributors/ard.html'
-    })
-
-    .when('/contributors/narwal', {
-      templateUrl: '/static/templates/contributors/narwal.html'
-    })
-
-    .when('/contributors/radhika', {
-      templateUrl: '/static/templates/contributors/radhika.html'
-    })
-
-    .when('/contributors/sangm', {
-      templateUrl: '/static/templates/contributors/sangm.html'
-    })
-
-    .when('/contributors/sean', {
-      templateUrl: '/static/templates/contributors/sean.html'
-    })
-
-     .when('/contributors/leek', {
-      templateUrl: '/static/templates/contributors/leek.html'
-    })
-
-    .when('/contributors/karan', {
-      templateUrl: '/static/templates/contributors/karan.html'
-    })
-
-    .when('/contributors/vaidehi', {
-      templateUrl: '/static/templates/contributors/vaidehi.html'
-    })
-
-    .when('/contributors/jason', {
-      templateUrl: '/static/templates/contributors/jason.html'
-    })
-
-    .when('/contributors/lucamatsumoto', {
-      templateUrl: '/static/templates/contributors/lucamatsumoto.html'
-    })
-
-    .when('/contributors/arcolife', {
-      templateUrl: '/static/templates/contributors/arcolife.html'
-    })
-
-    .when('/contributors/AditiNath', {
-      templateUrl: '/static/templates/contributors/aditinath.html'
-    })
-
-    .when('/contributors/ucerron', {
-      templateUrl: '/static/templates/contributors/ucerron.html'
-    })
-
-    .when('/contributors/AmitRakesh', {
-		templateUrl: '/static/templates/contributors/amitrakesh.html'
-    })
-
-    .when('/contributors/Milstein', {
-      templateUrl: '/static/templates/contributors/milstein.html'
-    })
-
-    .when('/contributors/bmoix', {
-      templateUrl: '/static/templates/contributors/bmoix.html'
-    })
-
-    .when('/contributors/vineet', {
-      templateUrl: '/static/templates/contributors/vineet.html'
-    })
-
-    .when('/contributors/damon', {
-      templateUrl: '/static/templates/contributors/damon.html'
-    })
-
-	.when('/contributors/rcompton', {
-      templateUrl: '/static/templates/contributors/rcompton.html'
-    })
-
-    .when('/contributors/hiroshi', {
-      templateUrl: '/static/templates/contributors/hiroshi.html'
-    })
-
-    .when('/contributors/niraj', {
-      templateUrl: '/static/templates/contributors/niraj.html'
-    })
-
-    .when('/contributors/abhinav', {
-      templateUrl: '/static/templates/contributors/abhinav.html'
-    })
-
-	.when('/contributors/Pabitra', {
-      templateUrl: '/static/templates/contributors/Pabitra.html'
-    })
-
-  	.when('/contributors/KajalGupta', {
-      templateUrl: '/static/templates/contributors/kajal.html'
-    })
-
-    .when('/contributors/pemmasani', {
-      templateUrl: '/static/templates/contributors/pemmasani.html'
-    })
-
-    .when('/contributors/ramachandracn', {
-      templateUrl: '/static/templates/contributors/ramachandracn.html'
-    })
-
-    .when('/contributors/saviaga', {
-      templateUrl: '/static/templates/contributors/saviaga.html'
-    })
-
-    .when('/contributors/pitkofsky', {
-      templateUrl: '/static/templates/contributors/pitkofsky.html'
-    })
-
-   .when('/contributors/paresh', {
-      templateUrl: '/static/templates/contributors/paresh.html'
-    })
-
-    .when('/contributors/msb', {
-      templateUrl: '/static/templates/contributors/msb.html'
-    })
-
-    .when('/contributors/ankita2992', {
-      templateUrl: '/static/templates/contributors/ankita.html'
-    })
-
-    .when('/contributors/savage', {
-      templateUrl: '/static/templates/contributors/savage.html'
-    })
-
-    .when('/contributors/mistercruz', {
-      templateUrl: '/static/templates/contributors/mistercruz.html'
-    })
-
-    .when('/contributors/arshiya', {
-      templateUrl: '/static/templates/contributors/arshiya.html'
-    })
-
-    .when('/contributors/vivekvinodh', {
-      templateUrl: '/static/templates/contributors/vivekvinodh.html'
-    })
-
-     .when('/contributors/kevin', {
-      templateUrl: '/static/templates/contributors/kevin.html'
-     })
-
-     .when('/contributors/vipul', {
-      templateUrl: '/static/templates/contributors/vipul.html'
-     })
-
-     .when('/contributors/khushali', {
-      templateUrl: '/static/templates/contributors/khushali.html'
-     })
-
-    .when('/contributors/riyakothari', {
-	  templateUrl: '/static/templates/contributors/riyakothari.html'
-     })
-	.when('/contributors/balaca', {
-    templateUrl: '/static/templates/contributors/balaca.html'
-     })
-    .when('/contributors/jeerel', {
-         templateUrl: '/static/templates/contributors/jeerel.html'
-     })
-
-     .when('/contributors/aditimithal', {
-         templateUrl: '/static/templates/contributors/aditimithal.html'
-     })
-
-     .when('/contributors/niab', {
-         templateUrl: '/static/templates/contributors/niab.html'
-     })
-
-    .when('/contributors/aginzberg', {
-        templateUrl: '/static/templates/contributors/aginzberg.html'
-    })
-    .when('/contributors/xiaoran', {
-        templateUrl: '/static/templates/contributors/xiaoran.html'
-    })
-
     .otherwise('/');
   }
 })();
