250a986714a186c7589b80567b97de98f6c15b7b
Durim Morina <drm.mrn@gmail.com>
2015-07-30 12:18:42 +0200

    remove buggy code from task feed

--- static/js/task-feed/controllers/task-feed.controller.js
+++ static/js/task-feed/controllers/task-feed.controller.js
@@ -29,43 +29,6 @@
       TaskFeed.getProjects().then(
         function success (data) {
             self.modules = data[0];
-          /*
-          var data = successData[0];
-          self.projects = data;
-          var modules = [];
-          var moduleTasks = [];
-          
-          // Get all modules
-          data.forEach(function (project) {
-            modules.push(project.modules[0]);
-          });
-          
-          // get all module tasks
-          modules.forEach(function (module) {
-            module.module_tasks.forEach(function (task) {
-              moduleTasks.push(task);
-            });
-          });
-
-          // Check if module tasks have any worker, if not, they then are free to attempt.
-          var freeTasks = {};
-          moduleTasks.forEach(function (task) {
-            if (task.task_workers.length === 0)
-            freeTasks[task.module] = task;
-          });
-
-          // Make an array of available modules and their correpsonding tasks map back to freetask.
-          var freeModules = [];
-          modules.forEach(function (mod) {
-            if (freeTasks.hasOwnProperty(mod.id)) {
-              freeModules.push(mod);
-            }
-          });
-
-          self.freeModules = freeModules;
-          self.freeTasks = _.toArray(freeTasks);
-          //console.log(self.freeModules, self.projects);
-          */
         },
         function error(errData) {
           self.error = errData[0].detail;
