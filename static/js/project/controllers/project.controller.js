e404915325defa66a44da4f92e2dd8e7c2276f4c
Adam Ginzberg <ginzberg@stanford.edu>
2015-08-04 16:51:19 -0700

    added requester_modules filter

--- static/js/project/controllers/project.controller.js
+++ static/js/project/controllers/project.controller.js
@@ -62,6 +62,11 @@
         self.myProjects = data[0];
       });
 
+      self.myModules=[];
+      Project.getRequesterModules().then(function(data){
+        self.myModules = data[0];
+      });
+
       self.getStatusName = getStatusName;
       self.monitor = monitor;
 
