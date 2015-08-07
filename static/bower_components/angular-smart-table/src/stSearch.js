feab7561e7a76c41c852f27b765a2382dc4e017a
nistala <nrohits@gmail.com>
2015-08-07 15:25:51 -0700

    adding bower components for build

---
+++ static/bower_components/angular-smart-table/src/stSearch.js
@@ -0,0 +1,42 @@
+ng.module('smart-table')
+  .directive('stSearch', ['stConfig', '$timeout', function (stConfig, $timeout) {
+    return {
+      require: '^stTable',
+      link: function (scope, element, attr, ctrl) {
+        var tableCtrl = ctrl;
+        var promise = null;
+        var throttle = attr.stDelay || stConfig.search.delay;
+
+        attr.$observe('stSearch', function (newValue, oldValue) {
+          var input = element[0].value;
+          if (newValue !== oldValue && input) {
+            ctrl.tableState().search = {};
+            tableCtrl.search(input, newValue);
+          }
+        });
+
+        //table state -> view
+        scope.$watch(function () {
+          return ctrl.tableState().search;
+        }, function (newValue, oldValue) {
+          var predicateExpression = attr.stSearch || '$';
+          if (newValue.predicateObject && newValue.predicateObject[predicateExpression] !== element[0].value) {
+            element[0].value = newValue.predicateObject[predicateExpression] || '';
+          }
+        }, true);
+
+        // view -> table state
+        element.bind('input', function (evt) {
+          evt = evt.originalEvent || evt;
+          if (promise !== null) {
+            $timeout.cancel(promise);
+          }
+
+          promise = $timeout(function () {
+            tableCtrl.search(evt.target.value, attr.stSearch || '');
+            promise = null;
+          }, throttle);
+        });
+      }
+    };
+  }]);
