e2b6366a8ad8dbb261b46d2f64dba9503a1ce6e8
maniksingh92 <maniksingh92@gmail.com>
2015-08-09 02:01:05 +0530

    add helper queryFilter service

---
+++ static/js/helpers/helpers.module.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular
+      .module('crowdsource.helpers', [
+        'crowdsource.helper.services'
+      ]);
+
+  angular
+      .module('crowdsource.helper.services', []);
+})();
