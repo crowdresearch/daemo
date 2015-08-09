48e96963272c73eb6e6405b24238158faa74226d
maniksingh92 <maniksingh92@gmail.com>
2015-08-09 19:55:42 +0530

    add isMicro and isMacro flags to Project controller

--- static/js/project/controllers/project.controller.js
+++ static/js/project/controllers/project.controller.js
@@ -59,6 +59,8 @@
       self.querySearch = function(query) {
         return helpersService.querySearch(self.currentProject.metadata.column_headers, query, false);
       };
+      self.currentProject.isMicro = (self.currentProject.microFlag === "micro");
+      self.currentProject.isMacro = (self.currentProject.microFlag === "macro");
 
       self.myProjects = [];
       Project.getRequesterProjects().then(function(data) {
