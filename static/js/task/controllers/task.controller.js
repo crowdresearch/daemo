d6b03da0703470d5a184d97244ed8c8ff4fac72e
Durim Morina <drm.mrn@gmail.com>
2015-08-10 20:15:16 +0200

    map and css added for task, buildHtml has been moved to service for re-use, templates are being rendered now

--- static/js/task/controllers/task.controller.js
+++ static/js/task/controllers/task.controller.js
@@ -6,10 +6,31 @@
     .controller('TaskController', TaskController);
 
 	TaskController.$inject = ['$scope', '$location', '$mdToast', '$log', '$http', '$routeParams',
-    'Task', 'Authentication'];
+    'Task', 'Authentication', 'Template', '$sce'];
 
-	function TaskController($scope, $location, $mdToast, $log, $http, $routeParams, Task, Authentication) {
+	function TaskController($scope, $location, $mdToast, $log, $http, $routeParams, Task, Authentication, Template, $sce) {
   	    var self = this;
+        self.taskData = null;
+        self.buildHtml = buildHtml;
+        activate();
+
+        function activate(){
+            var task_id = $routeParams.taskId;
+            Task.getTaskWithData(task_id).then(function success(data, status) {
+                    console.log(data[0]);
+                    self.taskData = data[0];
+                },
+                function error(data, status) {
+
+                }).finally(function () {
+
+                }
+            );
+        }
+        function buildHtml(item) {
+            var html = Template.buildHtml(item);
+            return $sce.trustAsHtml(html);
+        }
 	}
 
 })();
