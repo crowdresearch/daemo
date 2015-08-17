1b76f1abfed75fd3eeea4a61c4e16fc99827181a
Adam Ginzberg <ginzberg@stanford.edu>
2015-08-17 15:10:51 -0700

    merged migration

---
+++ crowdsourcing/migrations/0035_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crowdsourcing', '0033_auto_20150817_2056'),
+        ('crowdsourcing', '0034_auto_20150817_2049'),
+    ]
+
+    operations = [
+    ]
