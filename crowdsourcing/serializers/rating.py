a1c9c0bb2af0c64a77138c896fd1db010cbb046b
nistala <nrohits@gmail.com>
2015-08-23 23:40:34 -0700

    get backend working to accept ratings

--- crowdsourcing/serializers/rating.py
+++ crowdsourcing/serializers/rating.py
@@ -1,6 +1,16 @@
 from crowdsourcing import models
+from crowdsourcing.serializers.user import UserProfileSerializer
 from rest_framework import serializers
 
 class WorkerRequesterRatingSerializer(serializers.ModelSerializer):
+    origin = UserProfileSerializer(read_only=True)
+
     class Meta:
         model = models.WorkerRequesterRating
+        fields = ('id', 'origin', 'target', 'module', 'weight',
+                  'type', 'created_timestamp', 'last_updated')
+
+    def create(self, **kwargs):
+        print kwargs
+        rating = models.WorkerRequesterRating.objects.create(origin=kwargs['origin'], **self.validated_data)
+        return rating
--- crowdsourcing/viewsets/rating.py
+++ crowdsourcing/viewsets/rating.py
@@ -1,7 +1,6 @@
-from crowdsourcing.serializers.requester import *
-from crowdsourcing.serializers.project import *
+from crowdsourcing.serializers.requester import RequesterSerializer
+from crowdsourcing.serializers.user import UserProfileSerializer
 from rest_framework import status, viewsets
-from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from crowdsourcing.models import WorkerRequesterRating
 from crowdsourcing.serializers.rating import WorkerRequesterRatingSerializer
@@ -10,3 +9,12 @@ from crowdsourcing.serializers.rating import WorkerRequesterRatingSerializer
 class WorkerRequesterRatingViewset(viewsets.ModelViewSet):
     queryset = WorkerRequesterRating.objects.all()
     serializer_class = WorkerRequesterRatingSerializer
+
+    def create(self, request, *args, **kwargs):
+        wrr_serializer = WorkerRequesterRatingSerializer(data=request.data)
+        if wrr_serializer.is_valid():
+            wrr_serializer.create(origin=request.user.userprofile)
+            return Response({'status': 'Rating created'})
+        else:
+            return Response(wrr_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
