76f08b8d4be2f74cdf0bf4770841b1f29e6d1688
Durim Morina <drm.mrn@gmail.com>
2015-08-31 01:27:12 +0200

    fix return value of permission

--- crowdsourcing/permissions/task.py
+++ crowdsourcing/permissions/task.py
@@ -8,3 +8,4 @@ class HasExceededReservedLimit(permissions.BasePermission):
         if view.action == 'create' and TaskWorker.objects.filter(worker=request.user.userprofile.worker,
                                                                  task_status__in=[1, 5]).count() > 8:
             raise PermissionDenied(detail='You have exceeded maximum number of tasks in progress.')
+        return True
