464ea0b1000b522f0db709fdc706e2bbb5cbe859
Durim Morina <drm.mrn@gmail.com>
2015-08-30 19:29:43 +0200

    raise PermissionDenied with custom message

--- crowdsourcing/permissions/task.py
+++ crowdsourcing/permissions/task.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions
 from crowdsourcing.models import TaskWorker
+from rest_framework.exceptions import PermissionDenied
 
 class HasExceededReservedLimit(permissions.BasePermission):
-    message = 'You have exceeded maximum number of tasks in progress.'
 
     def has_permission(self, request, view):
-        if view.action == 'create':
-            return TaskWorker.objects.filter(worker=request.user.userprofile.worker, task_status__in=[1, 5]).count() < 9
+        if view.action == 'create' and TaskWorker.objects.filter(worker=request.user.userprofile.worker,
+                                                                 task_status__in=[1, 5]).count() > 8:
+            raise PermissionDenied(detail='You have exceeded maximum number of tasks in progress.')
