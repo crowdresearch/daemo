025e1995bd06d2f4980c1fa5a48ee94efba8db3b
Shirish Goyal <shirish.goyal@gmail.com>
2015-09-01 21:27:13 +0530

    Feedback on CSV

--- crowdsourcing/viewsets/csvmanager.py
+++ crowdsourcing/viewsets/csvmanager.py
@@ -30,9 +30,9 @@ class CSVManagerViewSet(ViewSet):
     def download_results(self, request, *args, **kwargs):
         module_id = request.query_params.get('module_id')
         task = Task.objects.filter(module=module_id)
-        task_serializer = TaskSerializer(task, many=True, fields=('data', 'task_workers'))
+        task_serializer = TaskSerializer(task, many=True, fields=('data', 'task_workers', 'comments'))
         tasks = task_serializer.data
-        column_headers = ['task_status', 'worker_alias', 'created', 'last_updated']
+        column_headers = ['task_status', 'worker_alias', 'created', 'last_updated', 'feedback']
         data_keys = eval(tasks[0]['data']).keys()
         for key in data_keys:
             column_headers.append(key)
@@ -47,11 +47,12 @@ class CSVManagerViewSet(ViewSet):
                     results.append(task_worker_result['result'])
                 item = {'id': task_worker['id'], 'data': task['data'], 'worker_alias': task_worker['worker_alias'],
                         'task_status': task_worker['task_status'], 'results': results,
-                        'created': task_worker['created_timestamp'], 'last_updated': task_worker['last_updated']}
+                        'created': task_worker['created_timestamp'], 'last_updated': task_worker['last_updated'],
+                        'feedback': ', '.join(map(lambda x: x['comment'].get('body',''), [comment for comment in task['comments']])) }
                 items.append(item)
         max_results = 0
         for item in items:
-            temp = [item['task_status'], item['worker_alias'], item['created'], item['last_updated']]
+            temp = [item['task_status'], item['worker_alias'], item['created'], item['last_updated'], item['feedback']]
             for key in data_keys:
                 temp.append(eval(item['data'])[key])
             num_results = 0
