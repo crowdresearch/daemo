4a6dc998c2d642e4cec75e5fba7faed8bad9e0e9
Durim Morina <drm.mrn@gmail.com>
2015-09-10 14:08:12 +0200

    mixins added

--- crowdsourcing/viewsets/experimental.py
+++ crowdsourcing/viewsets/experimental.py
@@ -5,9 +5,14 @@ from django.utils import timezone
 from rest_framework.permissions import IsAuthenticated
 from crowdsourcing.serializers.experimental import TaskRankingSerializer
 from crowdsourcing.experimental_models import TaskRanking
+from crowdsourcing.permissions.experimental import IsTaskWorker
+from crowdsourcing.permissions.user import IsWorker
+from rest_framework import mixins
 
 
-class TaskRankingViewSet(viewsets.ModelViewSet):
+class TaskRankingViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+                         mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = TaskRanking.objects.all()
     serializer_class = TaskRankingSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsWorker, IsTaskWorker]
+    lookup_field = 'task'
