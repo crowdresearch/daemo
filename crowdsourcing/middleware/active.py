691e22b02c0cdeac75c7fb1b175fed2d91028a32
dmorina <drm.mrn@gmail.com>
2015-11-05 15:35:24 -0800

    fix indentation

--- crowdsourcing/middleware/active.py
+++ crowdsourcing/middleware/active.py
@@ -2,9 +2,10 @@ import django.contrib.auth
 from crowdsourcing.models import UserProfile
 from datetime import datetime
 
+
 class CustomActiveViewMiddleware():
-	def process_view(self, request, callback, callback_args, callback_kwargs):
-		if not request.user.is_anonymous() and request.path == '/': 
-			userprofile = request.user.userprofile
-			userprofile.last_active = datetime.now()
-			userprofile.save()
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        if not request.user.is_anonymous() and request.path == '/':
+            userprofile = request.user.userprofile
+            userprofile.last_active = datetime.now()
+            userprofile.save()
