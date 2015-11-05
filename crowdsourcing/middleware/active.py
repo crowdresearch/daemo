0be10223b3b5ed8b1b40e84ae289779b83ff63bb
Adam Ginzberg <ginzberg@stanford.edu>
2015-11-04 17:07:58 -0800

    add last_active on login

--- crowdsourcing/middleware/active.py
+++ crowdsourcing/middleware/active.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class CustomActiveViewMiddleware():
 	def process_view(self, request, callback, callback_args, callback_kwargs):
-		if not request.user.is_anonymous() and request.path == '/':
+		if not request.user.is_anonymous() and request.path == '/': 
 			userprofile = request.user.userprofile
 			userprofile.last_active = datetime.now()
 			userprofile.save()
