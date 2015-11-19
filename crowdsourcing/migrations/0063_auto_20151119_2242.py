b3d797c3ccc633d2251d399895d78240c06401c4
dmorina <drm.mrn@gmail.com>
2015-11-19 14:55:58 -0800

    resolve merge conflicts

--- crowdsourcing/migrations/0063_auto_20151119_2242.py
+++ crowdsourcing/migrations/0063_auto_20151119_2242.py
@@ -7,12 +7,17 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('crowdsourcing', '0060_auto_20151016_1024'),
+        ('crowdsourcing', '0062_userprofile_last_active'),
     ]
 
     operations = [
         migrations.RemoveField(
             model_name='templateitem',
             name='sub_type',
+        ),
+        migrations.AddField(
+            model_name='templateitem',
+            name='label',
+            field=models.TextField(null=True, blank=True),
         ),
     ]
