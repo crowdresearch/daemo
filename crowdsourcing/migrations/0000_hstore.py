7c627b055e9eb07dc18517f68ceb8aa31dc295ea
dmorina <drm.mrn@gmail.com>
2015-12-07 14:06:58 -0800

    hstore

---
+++ crowdsourcing/migrations/0000_hstore.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+from django.contrib.postgres.operations import HStoreExtension
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('crowdsourcing', '0073_auto_20151204_2310'),
+    ]
+    # run_before = [
+    #     ('crowdsourcing', '0001_squashed_0063_auto_20151119_2242'),
+    # ]
+
+    operations = [
+        HStoreExtension(),
+    ]
