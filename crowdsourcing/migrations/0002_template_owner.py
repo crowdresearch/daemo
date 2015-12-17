0250bc6ed0bea917dfdca4bb586f14643446b64a
dmorina <drm.mrn@gmail.com>
2015-12-17 12:01:05 -0800

    owner migration fix

--- crowdsourcing/migrations/0002_template_owner.py
+++ crowdsourcing/migrations/0002_template_owner.py
@@ -16,7 +16,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='template',
             name='owner',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='crowdsourcing.UserProfile'),
-            preserve_default=False,
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='crowdsourcing.UserProfile')
         ),
     ]
