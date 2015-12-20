b52bad80b07ada154a6dbc70af0d4a7ca2edc750
Shirish Goyal <shirish.goyal@gmail.com>
2015-12-20 15:12:09 +0530

    flake8

--- csp/urls.py
+++ csp/urls.py
@@ -6,8 +6,9 @@ from crowdsourcing.viewsets.user import UserViewSet, UserProfileViewSet, UserPre
 from crowdsourcing.viewsets.requester import RequesterRankingViewSet, RequesterViewSet, QualificationViewSet
 from crowdsourcing.viewsets.rating import WorkerRequesterRatingViewset, RatingViewset
 from crowdsourcing.viewsets.worker import *
-from crowdsourcing.viewsets.task import TaskViewSet, CurrencyViewSet, TaskWorkerResultViewSet, TaskWorkerViewSet, ExternalSubmit
-from crowdsourcing.viewsets.template import TemplateViewSet, TemplateItemViewSet,TemplateItemPropertiesViewSet
+from crowdsourcing.viewsets.task import TaskViewSet, CurrencyViewSet, TaskWorkerResultViewSet, TaskWorkerViewSet, \
+    ExternalSubmit
+from crowdsourcing.viewsets.template import TemplateViewSet, TemplateItemViewSet, TemplateItemPropertiesViewSet
 from crowdsourcing.viewsets.drive import *
 from crowdsourcing.viewsets.google_drive import GoogleDriveOauth, GoogleDriveViewSet
 from crowdsourcing.viewsets.message import ConversationViewSet, MessageViewSet
@@ -50,18 +51,18 @@ router.register(r'api/finance', FinancialAccountViewSet)
 router.register(r'^api/file', FileViewSet)
 
 urlpatterns = patterns('',
-  url(r'^api/v1/auth/registration-successful',views.registration_successful),
-  url(r'^api/auth/login/$', views.Login.as_view()),
-  url(r'^api/auth/logout/$', views.Logout.as_view()),
-  url(r'^api/oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-  url(r'^api/oauth2-ng/token', views.Oauth2TokenView.as_view()),
-  url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-  url(r'^api/google-drive/init', GoogleDriveOauth.as_view({'post': 'auth_init'})),
-  url(r'^api/google-drive/finish', GoogleDriveOauth.as_view({'post': 'auth_end'})),
-  url(r'^api/google-drive/list-files', GoogleDriveViewSet.as_view({'get': 'query'})),
-  url(r'^api/done/$', ExternalSubmit.as_view()),
-  url(r'', include(router.urls)),
-  url('^.*$', views.home, name='home'),
-)
+                       url(r'^api/v1/auth/registration-successful', views.registration_successful),
+                       url(r'^api/auth/login/$', views.Login.as_view()),
+                       url(r'^api/auth/logout/$', views.Logout.as_view()),
+                       url(r'^api/oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+                       url(r'^api/oauth2-ng/token', views.Oauth2TokenView.as_view()),
+                       url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                       url(r'^api/google-drive/init', GoogleDriveOauth.as_view({'post': 'auth_init'})),
+                       url(r'^api/google-drive/finish', GoogleDriveOauth.as_view({'post': 'auth_end'})),
+                       url(r'^api/google-drive/list-files', GoogleDriveViewSet.as_view({'get': 'query'})),
+                       url(r'^api/done/$', ExternalSubmit.as_view()),
+                       url(r'', include(router.urls)),
+                       url('^.*$', views.home, name='home'),
+                       )
 
 urlpatterns += staticfiles_urlpatterns()
