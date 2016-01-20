88ff9bc8ec549674abfe0cc4b82e528df6b3401c
dmorina <drm.mrn@gmail.com>
2016-01-20 12:56:32 -0800

    local settings

--- local_settings_default.py
+++ local_settings_default.py
@@ -5,6 +5,7 @@ REGISTRATION_ALLOWED = True
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 BROKER_URL = 'redis://localhost:6379'
 CELERY_TIMEZONE = 'America/Los_Angeles'
+SITE_HOST = 'https://localhost:8000'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
