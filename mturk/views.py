24e863d70a0e67ee8c2b4b84774409e187f9df06
dmorina <drm.mrn@gmail.com>
2016-01-20 10:03:17 -0800

    cleanup view

--- mturk/views.py
+++ mturk/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 
 
 def mturk_index(request, *args, **kwargs):
-    from mturk.interface import MTurkProvider
-    from crowdsourcing.models import Project, Task
-    provider = MTurkProvider('https://localhost:8000')
-    project = Project.objects.get(id=1)
-    task = Task.objects.get(project_id=1)
-    provider.create_hits(project=project, tasks=[task])
     return render(request, 'mturk_index.html')
