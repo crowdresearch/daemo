07ddd6917748feed480c7978e1af59a417e7f566
Shirish Goyal <shirish.goyal@gmail.com>
2016-02-16 16:59:42 +0530

    handle dates

--- static/js/project/controllers/project.controller.js
+++ static/js/project/controllers/project.controller.js
@@ -38,6 +38,8 @@
 
                     if(self.project.deadline == null){
                         self.project.deadline = self.minDate;
+                    }else{
+                        self.project.deadline = convertDate(self.project.deadline);
                     }
                 },
                 function error(response) {
@@ -45,6 +47,20 @@
                 }
             ).finally(function () {
             });
+        }
+
+        function convertDate(value){
+            var regexIso8601 = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+            var match;
+            if (typeof value === "string" && (match = value.match(regexIso8601))) {
+                // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+                var milliseconds = Date.parse(match[0]);
+                if (!isNaN(milliseconds)) {
+                    return new Date(milliseconds);
+                }
+            }else{
+                return self.minDate;
+            }
         }
         
         function doPrototype() {
