1f2ccbe208dc5fff5c4db0bd51ea4c4e42715144
Shirish Goyal <shirish.goyal@gmail.com>
2016-02-16 18:22:34 +0530

    add error handling, improve names

--- static/js/task/directives/task.directive.js
+++ static/js/task/directives/task.directive.js
@@ -10,47 +10,57 @@
     'use strict';
     angular
         .module('crowdsource.task.directives')
-        .directive('timerDirective', timerDirective);
+        .directive('timerCounter', timerCounter);
 
-    function timerDirective($timeout) {
+    function timerCounter($timeout) {
         return {
             restrict: "A",
             scope: {
-                text: "=myText",
-            },              
-            link: function(scope, element, attributes){
-                    scope.$watch("text", function(val) {
-                        if (scope.text != null)
-                        {   
-                            countdown(0,scope.text);
-                        }                        
-                    });
-                    function countdown(minutes, seconds)
-                    {   var endTime, hours, mins, msLeft, time;
-                        function twoDigits( n )
-                        {
-                            return (n <= 9 ? "0" + n : n);
-                        }
-
-                        function updateTimer()
-                        {
-                            msLeft = endTime - (+new Date);
-                            if ( msLeft < 1000 ) {
-                                scope.timer = "Task Expired";
-                                $timeout.cancel();
-                            } else {
-                                time = new Date( msLeft );
-                                hours = time.getUTCHours();
-                                mins = time.getUTCMinutes();
-                                scope.timer = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-                                $timeout(updateTimer, time.getUTCMilliseconds() + 500);
-                                }
-                            }
-                            endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-                            updateTimer();
-                        }
+                secondsLeft: "="
             },
-            template: 'Time left : {{ timer }} '
+            link: function (scope, element, attributes) {
+                if (angular.isUndefined(attributes.secondsLeft)) {
+                    throw "seconds-left value is not passed";
+                }
+
+                scope.$watch("secondsLeft", function (val) {
+                    if (val != null) {
+                        var secondsLeft = parseInt(val);
+
+                        if (!isNaN(secondsLeft)) {
+                            countdown(0, secondsLeft);
+                        }else{
+                            throw "seconds-left value must be integer";
+                        }
+                    }
+                });
+
+                function countdown(minutes, seconds) {
+                    var endTime, hours, mins, msLeft, time;
+
+                    function twoDigits(n) {
+                        return (n <= 9 ? "0" + n : n);
+                    }
+
+                    function updateTimer() {
+                        msLeft = endTime - (+new Date);
+                        if (msLeft < 1) {
+                            scope.timer = "Expired";
+                            $timeout.cancel();
+                        } else {
+                            time = new Date(msLeft);
+                            hours = time.getUTCHours();
+                            mins = time.getUTCMinutes();
+                            scope.timer = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+                            $timeout(updateTimer, time.getUTCMilliseconds() + 500);
+                        }
+                    }
+
+                    endTime = (+new Date) + 1000 * (60 * minutes + seconds) + 500;
+                    updateTimer();
+                }
+            },
+            template: '{{ timer }}'
         };
     }
 })();
