fc2bbd842b357060dc0a8f7232fd99e8cf450e5a
dmorina <drm.mrn@gmail.com>
2016-02-22 14:41:38 -0800

    timeout to 72h

--- mturk/interface.py
+++ mturk/interface.py
@@ -67,7 +67,7 @@ class MTurkProvider(object):
                 qualifications = self.get_qualifications()
             if not MTurkHIT.objects.filter(task=task):
                 hit = self.connection.create_hit(hit_type=None, max_assignments=max_assignments,
-                                                 title=title, reward=reward, duration=datetime.timedelta(hours=12),
+                                                 title=title, reward=reward, duration=datetime.timedelta(hours=72),
                                                  description=self.description, keywords=self.keywords,
                                                  qualifications=qualifications,
                                                  question=question)[0]
