2f87a4d0191edfa260e48f2b076007b600e70db7
Adam Ginzberg <ginzberg@stanford.edu>
2016-04-29 10:52:58 -0700

    flake

--- crowdsourcing/tasks.py
+++ crowdsourcing/tasks.py
@@ -6,9 +6,9 @@ from datetime import datetime, timedelta
 
 @celery_app.task
 def expire_tasks():
-	TaskWorker.objects.filter(created_timestamp__lte=datetime.utcnow() -
-								timedelta(minutes=TASK_EXPIRATION_INTERVAL)).filter(
-								task_status=TaskWorker.STATUS_IN_PROGRESS).update(
-								task_status=TaskWorker.STATUS_EXPIRED)
-	return 'SUCCESS'
-								
+    TaskWorker.objects.filter(
+        created_timestamp__lte=datetime.utcnow() -
+        timedelta(minutes=TASK_EXPIRATION_INTERVAL)).filter(
+        task_status=TaskWorker.STATUS_IN_PROGRESS).update(
+        task_status=TaskWorker.STATUS_EXPIRED)
+    return 'SUCCESS'
