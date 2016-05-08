31f8259669b52135f594bf10469fc15ea8817f25
Shirish Goyal <shirish.goyal@gmail.com>
2016-05-08 06:44:38 +0530

    review assign permission

--- crowdsourcing/permissions/task.py
+++ crowdsourcing/permissions/task.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
-from crowdsourcing.models import TaskWorker
 from rest_framework.exceptions import PermissionDenied
+
+from crowdsourcing.models import TaskWorker
 
 
 class HasExceededReservedLimit(permissions.BasePermission):
@@ -8,4 +9,11 @@ class HasExceededReservedLimit(permissions.BasePermission):
         if view.action == 'create' and TaskWorker.objects.filter(worker=request.user.userprofile.worker,
                                                                  task_status__in=[1, 5]).count() >= 8:
             raise PermissionDenied(detail='You have exceeded maximum number of tasks in progress.')
+        return True
+
+
+class AlreadyAssigned(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if view.action == 'assign' and obj.reviewer is not None and obj.reviewer != request.user.userprofile.worker:
+            raise PermissionDenied(detail='This task is already assigned to someone and is in progress.')
         return True
