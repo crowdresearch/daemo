54a4701a70be0ce999ec142200d1687bf3699bdc
dmorina <drm.mrn@gmail.com>
2016-05-25 23:07:20 -0700

    cleanup closes #558

--- crowdsourcing/viewsets/file.py
+++ crowdsourcing/viewsets/file.py
@@ -8,7 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 import pandas as pd
 
 from crowdsourcing.serializers.file import BatchFileSerializer
-from crowdsourcing.models import BatchFile, Task, TaskWorker, TaskWorkerResult
+from crowdsourcing.models import BatchFile, TaskWorker, TaskWorkerResult
 
 
 class FileViewSet(mixins.RetrieveModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin, GenericViewSet):
@@ -59,48 +59,12 @@ class FileViewSet(mixins.RetrieveModelMixin, mixins.CreateModelMixin, mixins.Des
                     results[len(results) - 1].update(task_data)
             result_dict = self._to_dict(result)
             results[len(results) - 1].update(result_dict)
-
-            # task = Task.objects.filter(project=project_id)
-            # task_serializer = TaskSerializer(task, many=True, fields=('data', 'task_workers'))
-            # tasks = task_serializer.data
-            # column_headers = ['task_status', 'worker_alias', 'created', 'last_updated']
-            # data_keys = tasks[0]['data'].keys()
-            # for key in data_keys:
-            #     column_headers.append(key)
-            # data = []
-            # items = []
-            # for task in tasks:
-            #     task_workers = task['task_workers']
-            #     for task_worker in task_workers:
-            #         task_worker_results = task_worker['task_worker_results']
-            #         results = []
-            #         for task_worker_result in task_worker_results:
-            #             results.append(task_worker_result['result'])
-            #         item = {'id': task_worker['id'], 'data': task['data'], 'worker_alias': task_worker['worker_alias'],
-            #                 'task_status': task_worker['task_status'], 'results': results,
-            #                 'created': task_worker['created_timestamp'], 'last_updated': task_worker['last_updated']}
-            #         items.append(item)
-            # max_results = 0
-            # for item in items:
-            #     temp = [item['task_status'], item['worker_alias'], item['created'], item['last_updated']]
-            #     for key in data_keys:
-            #         if key in item['data']:
-            #             temp.append(item['data'][key])
-            #     num_results = 0
-            #     for result in item['results']:
-            #         num_results += 1
-            #         temp.append(result)
-            #     if num_results > max_results:
-            #         max_results = num_results
-            #     data.append(temp)
-            # for i in xrange(1, max_results + 1):
-            #     column_headers.append('Output_' + str(i))
-            df = pd.DataFrame(results)
-            output = StringIO.StringIO()
-            df.to_csv(output, columns=results[0].keys(), index=False, encoding="utf-8")
-            data = output.getvalue()
-            output.close()
-            return Response(data, status.HTTP_200_OK)
+        df = pd.DataFrame(results)
+        output = StringIO.StringIO()
+        df.to_csv(output, columns=results[0].keys(), index=False, encoding="utf-8")
+        data = output.getvalue()
+        output.close()
+        return Response(data, status.HTTP_200_OK)
 
     @staticmethod
     def _to_dict(result):
