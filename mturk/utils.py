5e97f2e6b0e2825c5e236ecb81df12a1b287a9f6
dmorina <drm.mrn@gmail.com>
2016-06-01 11:35:54 -0700

    flake8

--- mturk/utils.py
+++ mturk/utils.py
@@ -15,7 +15,7 @@ def get_or_create_worker(worker_id):
     if user is None:
         user_obj = User.objects.create(username=daemo_username, email=daemo_username + '@daemo.stanford.edu',
                                        password=make_password(generate_random_id()), is_active=False)
-        profile_obj = UserProfile.objects.create(user=user_obj)
+        UserProfile.objects.create(user=user_obj)
         return user_obj
     else:
         return user
