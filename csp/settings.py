642cc6a6115fd70a4f0c9d04d46d29c4145a5671
Shirish Goyal <shirish.goyal@gmail.com>
2016-09-01 13:28:47 -0700

    use chosen email backend for error emails

--- csp/settings.py
+++ csp/settings.py
@@ -336,7 +336,9 @@ LOGGING = {
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+            'email_backend': EMAIL_BACKEND,
         },
         'console': {
             'level': 'INFO',
