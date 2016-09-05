e4f24577053d4b136cf433f38f8a51e5984a3a1d
dmorina <drm.mrn@gmail.com>
2016-09-05 02:07:15 -0700

    post_mturk=True for review projects

--- crowdsourcing/serializers/project.py
+++ crowdsourcing/serializers/project.py
@@ -126,6 +126,7 @@ class ProjectSerializer(DynamicFieldsModelSerializer):
 
         review_project = models.Project.objects.create(name=project_name, owner=project.owner,
                                                        parent=project, is_prototype=False, min_rating=1.99,
+                                                       post_mturk=True,
                                                        is_review=True, deleted_at=timezone.now())
         if parent_review_project is not None:
             review_project.price = parent_review_project.price
