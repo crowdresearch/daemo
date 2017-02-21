04c68e560c3c57401d193dcd800a4c80482b4766
Shirish Goyal <shirish.goyal@gmail.com>
2017-02-21 15:35:04 -0800

    flake8

--- crowdsourcing/viewsets/payment.py
+++ crowdsourcing/viewsets/payment.py
@@ -1,4 +1,4 @@
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import list_route
 from rest_framework.permissions import IsAuthenticated
