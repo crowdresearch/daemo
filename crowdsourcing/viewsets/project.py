f4c2aa027f5879faead151a66983487d9444bb6b
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-07-10 13:20:49 -0700

    Add min_rating field to project API request

--- crowdsourcing/viewsets/project.py
+++ crowdsourcing/viewsets/project.py
@@ -67,7 +67,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
                                                'is_prototype', 'template', 'status', 'post_mturk',
                                                'qualification', 'group_id', 'revisions', 'task_time',
                                                'has_review', 'parent', 'hash_id', 'is_api_only', 'batch_files',
-                                               'aux_attributes', 'allow_price_per_task', 'task_price_field'),
+                                               'aux_attributes', 'allow_price_per_task', 'task_price_field', 'min_rating'),
                                        context={'request': request})
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
