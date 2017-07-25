cfe7403f248d8b2d77036800bb78d7db3e2f4c80
dmorina <drm.mrn@gmail.com>
2017-07-25 12:35:29 -0700

    disable browsableAPIRenderer

--- csp/settings.py
+++ csp/settings.py
@@ -54,7 +54,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
