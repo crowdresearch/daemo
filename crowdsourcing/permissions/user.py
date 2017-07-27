20fe683db8c62a5d5d64807b8ab8828bd7d3454d
Michael Bernstein <msb@cs.stanford.edu>
2017-07-27 10:10:43 -0700

    Updating registration error

--- crowdsourcing/permissions/user.py
+++ crowdsourcing/permissions/user.py
@@ -19,5 +19,5 @@ class CanCreateAccount(permissions.BasePermission):
         user_email = request.data.get('email')
         users = RegistrationWhitelist.objects.filter(email=user_email).count()
         if view.action == 'create' and not (request.user.is_staff or settings.REGISTRATION_ALLOWED) and users == 0:
-            raise PermissionDenied(detail='Registration is closed.')
+            raise PermissionDenied(detail='We are currently in closed beta. If you\'d like an account, email daemo@cs.stanford.edu with a short description of what you\'d like to use Daemo for.')
         return True
