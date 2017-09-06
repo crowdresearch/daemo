15aacfd495b1fcaa01a8c4d014ee3e1a60432235
dmorina <drm.mrn@gmail.com>
2017-09-05 17:29:09 -0700

    change email for signup

--- crowdsourcing/permissions/user.py
+++ crowdsourcing/permissions/user.py
@@ -20,6 +20,6 @@ class CanCreateAccount(permissions.BasePermission):
         users = RegistrationWhitelist.objects.filter(email=user_email).count()
         if view.action == 'create' and not (request.user.is_staff or settings.REGISTRATION_ALLOWED) and users == 0:
             raise PermissionDenied(detail='We are currently in closed beta. '
-                                          'If you\'d like an account, email daemo@cs.stanford.edu '
+                                          'If you\'d like an account, email support@daemo.org '
                                           'with a short description of what you\'d like to use Daemo for.')
         return True
